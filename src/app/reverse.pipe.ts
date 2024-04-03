e571b386b084cabcaee69bf6a419ed0d6fe0c105
ra2dd <radoslawpryw@gmail.com>
2024-04-03 12:39:55 +0200

    inital commit

---
+++ src/app/reverse.pipe.ts
@@ -0,0 +1,16 @@
+import {Pipe, PipeTransform} from '@angular/core';
+
+@Pipe({
+  standalone: true,
+  name: 'reverse'
+})
+export class ReversePipe implements PipeTransform {
+  transform(value: string): string {
+    let reverse = '';
+
+    for (let i = value.length - 1; i >= 0; i--) {
+      reverse += value[i]
+    }
+    return reverse;
+  }
+}
